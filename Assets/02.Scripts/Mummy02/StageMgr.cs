f4d460b8bbc6f0566a4f717e713fcf617d982b13
Jaehyun Lee <myleje@gmail.com>
2019-10-02 13:58:09 +0900

    .

--- Assets/02.Scripts/Mummy02/StageMgr.cs
+++ Assets/02.Scripts/Mummy02/StageMgr.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class StageMgr : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //싱글톤 변수 선언
+    public static StageMgr instance = null;
+    //버섯, 몬스터 프리팹 로드
+    private GameObject mushroomPrefab;
+    private GameObject monsterPrefab;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
     void Start()
     {
-        
+        mushroomPrefab = Resources.Load<GameObject>("MushRoom");
+        monsterPrefab  = Resources.Load<GameObject>("Monster");        
     }
 
     // Update is called once per frame
